66b9028d0a5e9422e47e38eafb72996e9b1d00c5
Goran988 <goran.988@hotmail.com>
2016-01-18 11:25:15 +0100

    book controller and jsp

---
+++ src/main/java/org/bildit/library/controllers/BookController.java
@@ -0,0 +1,29 @@
+package org.bildit.library.controllers;
+
+import javax.validation.Valid;
+
+import org.bildit.library.model.Book;
+import org.bildit.library.service.impl.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookServiceImpl bookService;
+
+	@RequestMapping(value = "/newbook", method = RequestMethod.POST)
+	public String newBook(Model model, @Valid @ModelAttribute("book") Book book, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "newbook";
+		}
+		return "home";
+
+	}
+}
