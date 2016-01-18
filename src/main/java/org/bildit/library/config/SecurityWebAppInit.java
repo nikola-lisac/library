20521eb52b65466dd5fb035ea6f37ddfadb99a49
OgnjenMisic <omg.gio@gmail.com>
2016-01-18 16:57:04 +0100

    initialized security filter chain

---
+++ src/main/java/org/bildit/library/config/SecurityWebAppInit.java
@@ -0,0 +1,7 @@
+package org.bildit.library.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInit extends AbstractSecurityWebApplicationInitializer {
+
+}
